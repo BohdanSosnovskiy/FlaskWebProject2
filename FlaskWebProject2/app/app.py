4bf44d2c60a591e362f40ff145d1bfa02850e5d3
BohdanSosnovskiy <40169231+BohdanSosnovskiy@users.noreply.github.com>
2019-04-24 14:54:06 +0100

    addeed html page

--- FlaskWebProject2/app/app.py
+++ FlaskWebProject2/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from tasks import add
 
 app = Flask(__name__)
@@ -7,15 +7,17 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def index():
-    return 'Welcome to project'
+    return render_template('index.html')
 
 list_result = []
 
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/new', methods=['POST'])
 def new_task():
-    result = add.apply_async(args=[10, 20])
-    list_result.append(result)
-    return result.id
+    if request.method == 'POST':
+        result = add.apply_async(args=[request.form['Text1'], request.form['Text2']])
+        list_result.append(result)
+        request.form['TextArea1'] = result.id
+        return result.id
 
 @app.route('/result/<id_result>')
 def result_task(id_result):
